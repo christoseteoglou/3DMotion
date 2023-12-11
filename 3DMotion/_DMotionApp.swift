0b9ddd3aa425a8712dcf2347b24c9d89ea04209c
christoseteoglou <itsmeglue@gmail.com>
2023-12-11 11:50:39 +0100

    Initial Commit

---
+++ 3DMotion/_DMotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DMotionApp.swift
+//  3DMotion
+//
+//  Created by Christos Eteoglou on 2023-12-11.
+//
+
+import SwiftUI
+
+@main
+struct _DMotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
